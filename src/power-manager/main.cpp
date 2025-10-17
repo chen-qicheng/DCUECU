23bde210f9802fb98917b76cb85adc76d5d3570f
chen-qicheng <chen.qicheng@qq.com>
2025-10-17 18:31:05 +0800

    feat(power-manager): 重构电源管理程序以支持多线程控制和设备初始化
    - 使用 gflags 和 glog 库优化配置与日志记录
    - 添加对电池供电、充电控制及按键监控的线程管理
    - 引入 DeviceFileHandler 类统一处理设备文件读写操作
    - 实现更精确的电池电压检测和充放电逻辑控制
    - 支持通过命令行参数设置关键延时时间等配置项

--- src/power-manager/main.cpp
+++ src/power-manager/main.cpp
@@ -1,321 +1,354 @@
 #include "main.h"
 
+#include <pthread.h>
+#include <signal.h>
+
+DEFINE_int32(keyDelayTime, 1400, "key delay time in seconds");
+int keyDelayTime;
+int lastKeyPressedTime = 0;
+
+// 线程相关变量
+pthread_t power_control_thread;
+pthread_t charging_control_thread;
+pthread_t key_monitor_thread;
+bool threads_running = true;
+
 int main (int argc,char *argv[])
 { 
-    // 初始化glog
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_logtostderr = true;  // 将日志输出到stderr
+    GflagsInit(argc, argv);
+    GlogInit();
+    DeviceInit();
 
+    LOG(INFO) << "start create threads...";
+    pthread_create(&power_control_thread, NULL, PowerControlThread, NULL);
+    pthread_create(&charging_control_thread, NULL, ChargingControlThread, NULL);
+    pthread_create(&key_monitor_thread, NULL, KeyMonitorThread, NULL);
 
-    time_t rawtime;
-    struct tm *timenow;
-    struct tm *timebegin;
-
-    int delytime =(argc > 1 )? (unsigned char) atoi(argv[ 1 ]) : 1400;
-
-    time(&rawtime);
-    timenow = localtime(&rawtime);
-    LOG(INFO) << "START RUN = " << time(NULL) << ", delytime =" << delytime << ", "
-              << "Day = "<< timenow->tm_year+1900 << "-" << timenow->tm_mon+1 << "-"<< timenow->tm_mday << ", "
-              << "Time = " << timenow->tm_hour << ":" << timenow->tm_min << ":" << timenow->tm_sec;
-
-
-    int     paramdelay        = delytime;
-    int     lstkeytm          = getlastkeytime( );
-    //串口6用于载波，不适用于低压集抄，需去掉，改为串口号2(红外和维护口复用)和串口号5用于485-3被抄和ESAM，。
-    int     lstttyS5          = GetttySRsNumx( 5 );
-    int     lstttyS2          = GetttySRsNumx( 2 );
-    bool    lstbatterystatus  = judgebatteries( );
-
-    time_t  lstoptm     = time( NULL );
-
-    if( lstbatterystatus == false )   //上电的时候，为电池供电
-    {
-        delytime = paramdelay;          //终端断电延时为基准时间，如果终端从外部供电转换到电池供电,延时时间提高到3倍
-    }
-    managebatteries( 1 );
-
-    do
+    LOG(INFO) << "start main loop...";
+    while(true) 
     {
         sleep(1);
-        ManageBattery( );
+    }
 
-        if( judgebatteries() )    //终端处于外部电源供电状态
-        {
-            LOG(INFO) << "终端处于外部电源供电状态" ;
-            if( lstbatterystatus == false )     //上一次处于电池供电状态
-            {
-                lstbatterystatus = true;          //
-                managebatteries( 1 );             //打开电池供电，以便外部电源中断供电时，能自动切换到电池供电
-            }
-            continue;
-        }
+    LOG(INFO) << "exit the program...";
+    threads_running = false;
+    pthread_join(power_control_thread, NULL);
+    pthread_join(charging_control_thread, NULL);
+    pthread_join(key_monitor_thread, NULL);
 
-        if( lstbatterystatus == true )    //判断终端是否从外部供电切换到内部供电的状态
-        {//供电状态切换时，初始化相应变量
-            lstkeytm = getlastkeytime( );
-            lstttyS5 = GetttySRsNumx( 5 );
-            lstttyS2 = GetttySRsNumx( 2 );
-            lstoptm  = time( NULL );
-            lstbatterystatus  = false;
-            delytime  = paramdelay * 3;
-            //powerofftm = time( NULL );    //纪录停电时刻
-        }
-        if(
-            lstkeytm != getlastkeytime( )  ||
-            lstttyS5 != GetttySRsNumx( 5 ) ||
-            lstttyS2 != GetttySRsNumx( 2 ) )
-        {
-            lstkeytm = getlastkeytime( );
-            lstttyS5 = GetttySRsNumx( 5 );
-            lstttyS2 = GetttySRsNumx( 2 );
-            lstoptm  = time( NULL );
-            //终端从外部电源供电到内部电池供电，开始时必须保证3倍的基准时间用于数据上报
-            //停电后有操作，将最后一次操作时间作为电池供电延迟时间的起点
-        }
-
-        float BatteryVolt = 0;
-        if(GetBatteryVoltage( &BatteryVolt )==false)
-        {
-            continue;
-        }
-
-        //printf( "LST =%ld delytime = %d>>>>>>>>>>>>\n", time( NULL ), delytime);//std::abs( time( NULL ) - lstoptm ), delytime);
-        LOG(INFO) << "curtime = " << time( NULL ) <<"delytime = "<< delytime << " BatteryVolt = " << BatteryVolt;	
-
-
-        if ((std::abs(time(NULL) - lstoptm) > delytime)||(BatteryVolt  < 4.3))
-        { 
-            timebegin = localtime(&lstoptm);
-            time(&rawtime);
-            timenow = localtime(&rawtime);
-            
-            LOG(INFO) << "lstoptm is " << lstoptm << ":s rawtime is " << rawtime;
-            LOG(INFO) << "Battery supply at = " << timebegin->tm_year+1900 << ":year "
-                      << timebegin->tm_mon+1 << ":month " << timebegin->tm_mday << ":day "
-                      << timebegin->tm_hour << ":hour " << timebegin->tm_min << ":min "
-                      << timebegin->tm_sec << ":s";
-            
-            LOG(INFO) << "Battery close at = " << timenow->tm_year+1900 << ":year "
-                      << timenow->tm_mon+1 << ":month " << timenow->tm_mday << ":day "
-                      << timenow->tm_hour << ":hour " << timenow->tm_min << ":min "
-                      << timenow->tm_sec << ":s";
-
-            LOG(INFO) << "Battery Gap is = " << std::abs(rawtime-lstoptm) << " seconds";
-
-            managebatteries( 0 );
-        
-            sleep( 60 );
-        }
-
-    }while( true );
-
-    // 关闭glog（释放资源）
     google::ShutdownGoogleLogging();
+    gflags::ShutDownCommandLineFlags();
+    
     return 0;
 }
 
-int getlastkeytime( )
+void GlogInit()
 {
-    char keytime[ 14 ]={ 0 };
-    int fd = open( "/proc/wfet/last_key_time", O_RDWR);
-    read( fd, keytime, 14);
-    close( fd );
+    google::InitGoogleLogging("power-manager");
 
-    char * cputype = getenv( "cputype" );
-    if ( !cputype )
-    {
-        char tmp[]="2410";
-        cputype = tmp;
+    std::string log_dir = "./glog_logs/";
+    struct stat dir_stat;
+    if (stat(log_dir.c_str(), &dir_stat) == -1) {
+        mkdir(log_dir.c_str(), 0755);
     }
 
-    if ( 0 == strcmp( cputype, "9260" ) )
-    return atoll( keytime );
-    else
-    return atoll( &keytime[ 8 ] );  //去除年
+    google::SetLogDestination(google::GLOG_INFO, log_dir.c_str());
+    google::EnableLogCleaner(30);
+    FLAGS_max_log_size = 10;
+
+    google::SetStderrLogging(google::GLOG_ERROR);
+    google::InstallFailureSignalHandler();
 }
 
-bool judgebatteries( )//判断电池状态（上电 断电）
+void GflagsInit(int argc, char* argv[])
 {
-    int fd;
-    //  unsigned char reg;
-    char bit_reg;
-    fd = open("/proc/wfet/poweroff_state", O_RDWR);
-    if (fd < 0)
-    {
-        LOG(ERROR) << "judgebatteries(): can not open poweroff_state";
-        return false;
-    }
-
-    read(fd, &bit_reg, 1);
-    close(fd);
-
-    return ( bit_reg == '1' ) ? false : true ;                  //电池在上电状态
+    gflags::SetUsageMessage(
+        "Power management program for device: "
+        "handles battery power, charging control and key monitoring."
+    );
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 }
 
-bool managebatteries(int flag)//控制电池(上电 断电)
+void DeviceInit()
 {
-    char * cputype = getenv( "cputype" );
-    if ( !cputype )
-    {
-        char tmp[]="2410";
-        cputype = tmp;
-    }
-    if ( 0 == strcmp( cputype, "9260" ) )
-    {
-        SetSupplyStatus( flag );
-    }
-    else
-    {
-        unsigned char writebuff[2]={0};
-        if( flag == 1 )
-        {
-            writebuff[0] = 0xF7;//掩码 开放第4位
-            writebuff[1] = 0xff;//第四位写1,表示打开电池电源
-            LOG(INFO) << "打开电池电源: writebuff[0]= " << writebuff[0] << "writebuff[1]= "<< writebuff[1];//
-        }
-        else
-        {
-            writebuff[0] = 0xF7;//掩码 开放第4位
-            writebuff[1] = 0x00;//第四位写0,表示关闭电池电源
-            LOG(INFO) << "关闭电池电源: writebuff[0]= " << writebuff[0] << ",writebuff[1]= "<< writebuff[1];//
-        }
+    keyDelayTime = FLAGS_keyDelayTime;
+    EnableBatteryPowered();
+}
 
-        int fd = open("/dev/wfet1000_new_gpio", O_RDWR);
-        LOG(INFO) << "managebatteries flag= " << flag;
-
-        if (fd < 0)
-        {
-            LOG(ERROR) <<"managebatteries: can not open wfet1000_new_gpio";
-            return false;
-        }
-
-        write(fd,writebuff,2);
-        close(fd);
+void* PowerControlThread(void* arg)
+{
+    LOG(INFO) << "Power Control thread started";
+    
+    while(threads_running)
+    {
+        ControlBatteryPower();
+ 
+        sleep(1);
     }
+    
+    LOG(INFO) << "Power Control thread stopped";
+    return NULL;
+}
+
+void ControlBatteryPower()
+{ 
+    if(IsExternalPowered() && IsJustSwitchedToExternalPower())
+    {
+        EnableBatteryPowered();
+    }
+
+    if(IsBatteryPowered())
+    {
+        if(IsLongTimeNoKeyPressed() || IsBatteryVoltageLow())
+        {
+            DisableBatteryPowered();
+        }
+    }
+}
+
+void* ChargingControlThread(void* arg)
+{
+    LOG(INFO) << "Charging control thread started";
+    
+    while(threads_running)
+    {
+        ControlBatteryCharging();
+        sleep(5);
+    }
+    
+    LOG(INFO) << "Charging control thread stopped";
+    return NULL;
+}
+
+void ControlBatteryCharging()
+{
+    if(IsMinChargingVoltageReached() && !IsCharging()){
+        EnableCharging();
+    }
+
+    if(IsMaxChargingVoltageReached()){
+        DisableCharging();
+    }
+}
+
+void* KeyMonitorThread(void* arg)
+{
+    LOG(INFO) << "Monitor key thread started";
+
+    while(threads_running)
+    { 
+        if(lastKeyPressedTime != GetLastKeyTime())
+        {
+            lastKeyPressedTime = GetLastKeyTime();
+        }
+        sleep(1);
+    }
+
+    LOG(INFO) << "Monitor key thread stopped";
+    return NULL; 
+}
+
+bool IsJustSwitchedToExternalPower()
+{
+    static bool batteryPoweredStatus = IsBatteryPowered();
+    bool prevBatteryPoweredStatus = batteryPoweredStatus;
+
+    batteryPoweredStatus = IsBatteryPowered();
+    return prevBatteryPoweredStatus && !batteryPoweredStatus;
+}
+
+
+bool IsLongTimeNoKeyPressed()
+{
+    return (time(NULL) - lastKeyPressedTime) > keyDelayTime;
+}
+
+bool IsBatteryVoltageLow()
+{
+    float batteryVoltage = GetBatteryVoltage();
+    return batteryVoltage < MIN_BATTERY_POWERED_VOLTAGE;
+}
+
+bool IsMinChargingVoltageReached()
+{ 
+    float voltage = GetBatteryVoltage();
+    return voltage < MIN_CHARGING_VOLTAGE;
+}
+
+bool IsMaxChargingVoltageReached()
+{
+    float voltage = GetBatteryVoltage();
+    return  voltage > MAX_CHARGING_VOLTAGE;
+}
+
+void EnableCharging()
+{
+    SetChargeStatus(1);
+}
+
+void DisableCharging()
+{
+    SetChargeStatus(0);
+}
+
+
+int GetLastKeyTime()
+{
+    char keyTime[14] = {0};
+
+    DeviceFileHandler devFile("/proc/wfet/last_key_time");
+    devFile.GetData( keyTime, 14 );
+
+    LOG(INFO) << "keyTime = " << keyTime;
+    return atoll( keyTime );
+}
+
+bool IsBatteryPowered()
+{
+    char buffer;
+    DeviceFileHandler devFile("/proc/wfet/poweroff_state");
+    devFile.GetData( &buffer, 1 );
+    
+    return ( buffer == '1' ) ? true : false;
+}
+
+bool IsExternalPowered() 
+{
+    return !IsBatteryPowered();
+}
+
+
+bool EnableBatteryPowered()
+{
+    char writebuff[2]={0};
+
+    writebuff[0] = 0xF7;//掩码 开放第4位
+    writebuff[1] = 0xff;//第四位写1,表示打开电池电源
+    LOG(INFO) << "打开电池电源: writebuff[0]= 0xF7, writebuff[1]= 0xff";
+
+    DeviceFileHandler devFile("/dev/wfet1000_new_gpio");
+    if (!devFile.IsOpen()) return false;
+
+    devFile.SetData(writebuff, 2);
     return true;
 }
 
-int GetttySRsNumx( char id )
+bool DisableBatteryPowered()
 {
-    char temp_filestr[ 1024 ]={ 0 };
+    char writebuff[2]={0};
 
-    int fd = open("/proc/tty/driver/atmel_serial",O_RDWR);
-    if( fd < 0 ) return 0;
+    writebuff[0] = 0xF7;//掩码 开放第4位
+    writebuff[1] = 0x00;//第四位写0,表示关闭电池电源
+    LOG(INFO) << "关闭电池电源: writebuff[0]= 0xF7" << ",writebuff[1]= 0x00";
 
-    read( fd, temp_filestr, 1024);
-    close( fd );
+    DeviceFileHandler devFile("/dev/wfet1000_new_gpio");
+    if (!devFile.IsOpen()) return false;
 
-    char tmpid[ 10 ] = { 0 };
-    sprintf( tmpid, "%d:", id );
-
-
-    char* pstr = strstr( temp_filestr, tmpid );
-    if( pstr == NULL ) return 0;
-
-
-    char *startP = strstr( pstr, "rx:" );
-    if( startP == NULL ) return 0;
-
-    int num = 0;
-    sscanf( &startP[ 3 ], "%d", &num );
-    return num;
-}
-
-bool GetBatteryVoltage( float *pVale )
-{
-    char temp[ 32 ]={ 0 };
-
-    int fd = open("/proc/wfet/battery_adc",O_RDWR);
-    if( fd < 0 ) return false;
-
-    read( fd, temp, sizeof( temp ));
-    close( fd );
-    int vale = 0;
-    sscanf( temp, "%x", &vale );
-    *pVale = vale * 9.9 / 0x3ff;
-    LOG(INFO) << "Battary Vale = " << vale << "Vol = " << *pVale;
+    devFile.SetData(writebuff, 2);
     return true;
 }
 
-void ManageBattery( )
+float GetBatteryVoltage()
 {
-    static time_t lstgetvoltm = 0;
-    static int  circle = 6;
-    time_t curtm = time( NULL );
+    DeviceFileHandler devFile("/proc/wfet/battery_adc");
 
-    LOG(INFO) << " Span = " << abs( curtm - lstgetvoltm ) << ", circle = " << circle;
+    char buffer[32] = {0};
+    devFile.GetData( buffer, sizeof( buffer ));
 
-    if( abs( curtm - lstgetvoltm ) < circle ) return; //定时器
-    lstgetvoltm = curtm;
+    float voltage = -1.0;
+    int adcValue = 0;
+    sscanf( buffer, "%x", &adcValue );
 
-    float batteeyvol = 0;
-    if( !GetBatteryVoltage( &batteeyvol ) ) return; //读电池电压
+    voltage = adcValue * 9.9 / 0x3ff;
+    LOG(INFO) << "Battary adcValue = " << adcValue << "voltage = " << voltage;
+    return voltage;
+}
 
-    bool  IsChargeABattery = GetChargeStatus( );
 
-    if( batteeyvol < MINCHARGEVOL )       //电池电压低，需要充电
-    {
-        if( !IsChargeABattery )         ///不是充电状态，则控制充电
-        {
-            SetChargeStatus( 1 );
-        }
-        circle = 10;
-    }
+bool IsCharging()
+{
+    DeviceFileHandler devFile( "/proc/wfet/battery_charge" );
 
-    //电压在5.6～5.8之间，认为充电完成，停止充电
-    if( batteeyvol >  MINCHARGEVOL && batteeyvol < NOBATTRYVOL )
-    { //关闭充电电源
-        SetChargeStatus( 0 );
-        circle = 6;
-        return;
+    char buffer[16] = {0};
+    devFile.GetData( buffer, sizeof( buffer ));
+
+    int chargeStatus = 0;
+    sscanf(buffer, "%d", &chargeStatus);
+    LOG(INFO) << "ChargeStatus = " << chargeStatus ;
+
+    return (chargeStatus != 0); 
+}
+
+
+void SetChargeStatus( int enable )
+{
+    char status = (enable != 0) ? '1' : '0';
+
+    DeviceFileHandler devFile( "/proc/wfet/battery_charge" ); 
+    devFile.SetData(&status, sizeof(status));
+}
+
+
+
+
+
+DeviceFileHandler::DeviceFileHandler( const char * fileName )
+    : m_fileName( fileName ), m_fd(-1)
+{
+    m_fd = open(m_fileName.c_str(), O_RDWR);
+}
+
+DeviceFileHandler::~DeviceFileHandler()
+{
+    if (m_fd >= 0) {
+        close(m_fd);
     }
 }
 
-bool GetChargeStatus( )
+bool DeviceFileHandler::Open()
 {
-    char temp[ 16 ]={ 0 };
-    int fd = open( "/proc/wfet/battery_charge", O_RDWR );
-    if( fd < 0 ) return false;
-
-    read( fd, temp, sizeof( temp ));
-    close( fd );
-    int vale = 0;
-    sscanf( temp, "%d", &vale );
-    LOG(INFO) << "ChargeStatus = " <<  vale;
-    return vale;
+    if (m_fd >= 0) {
+        close(m_fd);
+    }
+    
+    m_fd = open(m_fileName.c_str(), O_RDWR);
+    return (m_fd >= 0);
 }
 
-void SetChargeStatus( int flags )
+bool DeviceFileHandler::IsOpen()
 {
-    char temp;
-    if ( flags )
-    {
-        temp = '1';
-    }
-    else
-    {
-        temp = '0';
-    }
-    int fd = open( "/proc/wfet/battery_charge", O_RDWR );
-    if( fd < 0 ) return;
-    write( fd, &temp, sizeof( char ) );
-    close( fd );
+    return (m_fd >= 0);
 }
 
-void SetSupplyStatus( int flags )
+int DeviceFileHandler::GetFd()
 {
-    char temp;
-    if ( flags )
-    {
-        temp = '1';
+    return m_fd;
+}   
+
+int DeviceFileHandler::GetData( char * data, int size )
+{
+    if (!data || size <= 0 || m_fd < 0) {
+        return -1;
     }
-    else
-    {
-        temp = '0';
+
+    ssize_t bytesRead = read(m_fd, data, size - 1);
+    if (bytesRead < 0) {
+        return -1;
     }
-    int fd = open( "/proc/wfet/battery_supply", O_RDWR );
-    if( fd < 0 ) return;
-    write( fd, &temp, sizeof( char ) );
-    close( fd );
+    
+    data[bytesRead] = '\0';
+    return bytesRead;
+}
+
+int DeviceFileHandler::SetData( const char * data, int size )
+{
+    if (!data || size <= 0 || m_fd < 0) {
+        return -1;
+    }
+
+    ssize_t bytesWritten = write(m_fd, data, size);
+    if (bytesWritten < 0) {
+        return -1;
+    }
+    return bytesWritten;
 }
--- src/power-manager/main.h
+++ src/power-manager/main.h
@@ -12,22 +12,63 @@
 #include <string.h>
 #include <stdarg.h>
 
+#include "gflags/gflags.h"
 #include "glog/logging.h"
 
-#define LOWBATTERYVOL   4.0  //4.9
-#define LOWVOLBACKUP    4.0  //4.95
-#define NOBATTRYVOL     5.8     ////充电时如果没有电池，则电压最小为6.0v
-#define MINCHARGEVOL    5.6       //最小充电电压，电池电压小于某一数值时启动充电
+#define MIN_BATTERY_POWERED_VOLTAGE   4.0
+#define MIN_CHARGING_VOLTAGE    5.6       //最小充电电压
+#define MAX_CHARGING_VOLTAGE    5.8
 
-bool judgebatteries();
-bool managebatteries(int flag);
-bool GetBatteryVoltage( float *pVale );
-bool GetChargeStatus( );
-void SetChargeStatus( int flags );
-void SetSupplyStatus( int flags );
-void ManageBattery( );
-int  getlastkeytime( );
-int GetttySRsNumx( char id );
+
+void GlogInit();
+void GflagsInit(int argc, char* argv[]);
+void DeviceInit();
+
+void* PowerControlThread(void* arg);
+void ControlBatteryPower();
+void* ChargingControlThread(void* arg);
+void ControlBatteryCharging();
+void* KeyMonitorThread(void* arg);
+
+bool IsJustSwitchedToExternalPower();
+bool IsLongTimeNoKeyPressed();
+bool IsBatteryVoltageLow();
+bool IsMinChargingVoltageReached();
+bool IsMaxChargingVoltageReached();
+
+void EnableCharging();
+void DisableCharging();
+
+int GetLastKeyTime();
+bool IsBatteryPowered();
+bool IsExternalPowered();
+
+bool EnableBatteryPowered();
+bool DisableBatteryPowered();
+float GetBatteryVoltage();
+bool IsCharging();
+
+void SetChargeStatus(int enable);
+
+
+
+class DeviceFileHandler
+{ 
+    public:
+    DeviceFileHandler() : m_fd(-1){}
+    DeviceFileHandler(const char * fileName );
+    ~DeviceFileHandler();
+
+    bool Open();
+    bool IsOpen();
+    int GetFd();
+    int GetData( char * data, int size );
+    int SetData( const char * data, int size );
+
+    private:
+        std::string m_fileName;
+        int m_fd;
+};
 
 
 #endif // !__MAIN_H__
